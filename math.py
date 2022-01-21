8726eb252573517c5cd1477e2b432d4aed86028e
tejasBadjate <87329998+tejasBadjate@users.noreply.github.com>
2022-01-21 10:53:12 +0530

    Initial commit of math.py file

---
+++ math.py
@@ -0,0 +1,19 @@
+#Add implementation
+def add(x,y):
+    return x+y
+#substract implementation
+def substract(x,y):
+    return x-y
+#multiply implementation
+def multiply(x,y):
+    return x*y
+#divide implementation
+def divide(x,y):
+
+    if y==0:
+	return "Divide_by_zero_error"
+    else:
+	return x/y
+#implementation
+def square(x):
+    return x*x
